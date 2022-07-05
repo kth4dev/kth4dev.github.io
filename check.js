cb3a7ad9ccb7a168bba4a7ed3f66b09d6e2dbe8c
HTUT IT <kth4dev@gmail.com>
2022-07-05 22:43:59 +0630

    loading

--- check.js
+++ check.js
@@ -1,9 +1,14 @@
+function getParameters() {
 
+  const searchParams = new URLSearchParams(window.location.search);
+  var productId=searchParams.get("product");
+  var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
+  window.location.replace(url);
+  setTimeout(hideLoading, 1000);
+}
 
-    function getParameters() {
-        const searchParams = new URLSearchParams(window.location.search);
-        var productId=searchParams.get("product");
-        var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
-          window.location.replace(url);
-    }
+function hideLoading(){
+  var element = document.getElementById("page_loader");
+  element.style.display = "none";
+}
 
