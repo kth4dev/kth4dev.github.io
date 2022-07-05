b67d019f7fa882e38e79337a62eebe089d62b730
HTUT IT <kth4dev@gmail.com>
2022-07-05 21:06:20 +0630

    delete

--- check.js
+++ check.js
@@ -4,7 +4,6 @@
         const searchParams = new URLSearchParams(window.location.search);
         var productId=searchParams.get("product");
         var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
-        document.getElementById("q").value=productId;
           window.location.replace(url);
     }
 
